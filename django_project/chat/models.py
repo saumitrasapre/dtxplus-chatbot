44a3361ea25476fa9bf43d055f50350a016f6e24
saumitrasapre <saumitrasapre69@gmail.com>
2024-10-01 21:45:02 -0700

    Added Chats model

--- django_project/chat/models.py
+++ django_project/chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Chat(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    chat_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+
--- django_project/chat/views.py
+++ django_project/chat/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Chat
 
 # Create your views here.
 
-posts = [
-    {
-        'author':'user1',
-        'title':'Post 1',
-        'content':'First post content',
-        'date_posted':'August 27, 2024',
-    },
-    {
-        'author':'user2',
-        'title':'Post 2',
-        'content':'Second post content',
-        'date_posted':'August 28, 2024',
-    },
-]
-
-
 def home(request):
     context = {
-        'posts': posts
+        'chats': Chat.objects.all()
     }
     return render(request,'chat/home.html', context)
 
